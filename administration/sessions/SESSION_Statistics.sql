cf90145833c9c93d4d0627a2263709863ff1221f
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-20 17:32:11 +0100

    Update SESSION_Statistics.sql

--- administration/sessions/SESSION_Statistics.sql
+++ administration/sessions/SESSION_Statistics.sql
@@ -1,1 +1,45 @@
 
+--------------------------
+--Session Stats By Session
+--for all sessions
+--------------------------
+
+-SESSION STAT NOTES:
+-Username - Name of the user
+-SID - Session ID
+-Statistic - Name of the statistic
+-Usage - Usage according to Oracle
+
+select  nvl(ss.USERNAME,'ORACLE PROC') username,
+	se.SID,
+	sn.NAME stastic,
+	VALUE usage
+from 	v$session ss, 
+	v$sesstat se, 
+	v$statname sn
+where  	se.STATISTIC# = sn.STATISTIC#
+and  	se.SID = ss.SID
+and	se.VALUE > 0
+order  	by sn.NAME, se.SID, se.VALUE desc
+;
+
+
+
+-----------------------------------------------------------------------------------
+-- Description  : Displays current session-specific statistics
+-- Requirements : Access to the V$ views.
+-- --------------------------------------------------------------------------------
+SET VERIFY OFF
+
+SELECT sn.name, 
+       ss.value
+FROM   v$sesstat ss,
+       v$statname sn,
+       v$session s
+WHERE  ss.statistic# = sn.statistic#
+AND    s.sid = ss.sid
+AND    s.audsid = SYS_CONTEXT('USERENV','SESSIONID')
+AND    sn.name LIKE '%' || DECODE(LOWER('&1'), 'all', '', LOWER('&1')) || '%';
+
+
+
