0b72153fca88230b4afdc2570d92acd3f4f7c200
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-20 22:01:51 +0100

    Update TBS_Create.sql

--- administration/storage/tablespaces/TBS_Create.sql
+++ administration/storage/tablespaces/TBS_Create.sql
@@ -1,1 +1,7 @@
 
+
+SELECT value from v$parameter where name = 'db_create_file_dest';
+
+CREATE tablespace ABIDI datafile size 1M;
+
+CREATE tablespace LASER datafile SIZE 5M AUTOEXTEND OFF EXTENT MANAGEMENT LOCAL AUTOALLOCATE;
