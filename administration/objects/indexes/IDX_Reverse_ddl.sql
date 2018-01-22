5d1877514f50ec72cc39b9db0930a39951b87846
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-22 12:57:03 +0100

    Update IDX_Reverse_ddl.sql

--- administration/objects/indexes/IDX_Reverse_ddl.sql
+++ administration/objects/indexes/IDX_Reverse_ddl.sql
@@ -1,8 +1,43 @@
-SET LONG 90000 LONGCHUNKSIZE 20000 PAGESIZE 0 LINESIZE 1000 FEEDBACK OFF VERIFY OFF TRIMSPOOL ON
-SELECT DBMS_METADATA.GET_DDL('INDEX',object_name,'&owner') FROM DBA_OBJECTS WHERE OWNER=UPPER('&owner') AND OBJECT_TYPE='INDEX';
+
+
+
+SET LONG 20000 LONGCHUNKSIZE 20000 PAGESIZE 0 LINESIZE 1000 FEEDBACK OFF VERIFY OFF TRIMSPOOL ON
+BEGIN
+   DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'SQLTERMINATOR', true);
+   DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'PRETTY', true);
+END;
+/
+SELECT DBMS_METADATA.get_ddl ('INDEX', index_name, table_owner)
+FROM   user_indexes
+WHERE  table_owner = UPPER('&owner_name');
+
+
 
 SET LONG 90000 LONGCHUNKSIZE 20000 PAGESIZE 0 LINESIZE 1000 FEEDBACK OFF VERIFY OFF TRIMSPOOL ON
-select DBMS_METADATA.GET_DDL('INDEX',object_name,'&owner') from dba_objects where owner=upper('&owner') and object_type ='INDEX' and object_name='&index';
+BEGIN
+   DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'SQLTERMINATOR', true);
+   DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'PRETTY', true);
+END;
+/
+SELECT DBMS_METADATA.GET_DDL('INDEX',object_name,'&owner') 
+FROM DBA_OBJECTS 
+WHERE OWNER=UPPER('&owner') 
+AND OBJECT_TYPE='INDEX';
+
+
+
+
+SET LONG 90000 LONGCHUNKSIZE 20000 PAGESIZE 0 LINESIZE 1000 FEEDBACK OFF VERIFY OFF TRIMSPOOL ON
+BEGIN
+   DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'SQLTERMINATOR', true);
+   DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'PRETTY', true);
+END;
+/
+SELECT DBMS_METADATA.GET_DDL('INDEX',object_name,'&owner') 
+FROM dba_objects 
+WHERE owner=upper('&owner') 
+AND object_type ='INDEX' 
+AND object_name='&index';
 
 
 
