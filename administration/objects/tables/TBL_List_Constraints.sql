8b08cec55377e149a242142db5e62bada2ee4b50
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-22 14:22:28 +0100

    Update TBL_List_Constraints.sql

--- administration/objects/tables/TBL_List_Constraints.sql
+++ administration/objects/tables/TBL_List_Constraints.sql
@@ -1,3 +1,33 @@
+
+
+----------------------------------------------------------------------------------------------
+--Script permettant de lister les Contraintes d'intégrités à désactiver
+--ici sur la table ANALYSE et pour le user Scott
+---------------------------------------------------------------------------------------------- 
+
+SELECT   TABLE_NAME,
+             CONSTRAINT_NAME,
+             STATUS,
+             CONSTRAINT_TYPE
+      FROM   ALL_CONSTRAINTS
+     WHERE       CONSTRAINT_TYPE = 'C'
+             AND OWNER = UPPER ('SCOTT')
+             AND TABLE_NAME = UPPER ('ANALYSE')
+             AND STATUS = 'ENABLED';
+
+TABLE_NAME                     CONSTRAINT_NAME                STATUS   C
+------------------------------ ------------------------------ -------- -
+ANALYSE                        SYS_C0013527                   ENABLED  C
+ANALYSE                        CHECK_A_PRE                    ENABLED  C
+ANALYSE                        CHECK_VER_PRE                  ENABLED  C
+ANALYSE                        CHECK_ANA_E_PR                 ENABLED  C
+ANALYSE                        CHECK_ANA_VAL_REC              ENABLED  C
+
+
+
+
+
+
 REPHEADER PAGE CENTER 'LISTE DES CONTRAINTES D''UNE TABLE OU VIEW'
 
 SET LINESIZE 100
