ac3a9d4be28650966d434cbfa4138263e8cd55c5
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-22 09:03:57 +0100

    Update AUD_User_Trigger.sql

--- administration/security/audit/AUD_User_Trigger.sql
+++ administration/security/audit/AUD_User_Trigger.sql
@@ -1,1 +1,34 @@
 
+
+CREATE TRIGGER emp_audit AFTER
+  INSERT OR
+  DELETE OR
+  UPDATE ON emp FOR EACH row BEGIN
+  INSERT
+  INTO emp_audit
+    (
+      empno,
+      ename,
+      job,
+      mgr,
+      hiredate,
+      sal,
+      comm,
+      deptno,
+      last_modified_user,
+      last_modified_date
+    )
+    VALUES
+    (
+      :new.empno,
+      :new.ename,
+      :new.job,
+      :new.mgr,
+      :new.hiredate,
+      :new.sal,
+      :new.comm,
+      :new.deptno,
+      USER,
+      sysdate
+    );
+END;
