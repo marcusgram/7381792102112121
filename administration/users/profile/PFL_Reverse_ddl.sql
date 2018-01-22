80da1179e98b57aa10c8668da419cd6c7e805b6a
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-22 13:05:36 +0100

    Create PFL_Reverse_ddl.sql

---
+++ administration/users/profile/PFL_Reverse_ddl.sql
@@ -0,0 +1,15 @@
+set heading off
+set feedback off
+set linesize 1000
+set trimspool on
+set verify off
+set termout off
+set embedded on
+set long 200000
+set pages 0
+SELECT DBMS_METADATA.GET_DDL('PROFILE', pr.name) ddl_string
+FROM (SELECT DISTINCT pi.name 
+FROM sys.profname$ pi
+WHERE pi.name != 'DEFAULT') pr
+/
+
