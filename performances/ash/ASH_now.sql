d561333afeaf71536b31f76c2bcd4aff2e19461b
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-23 16:11:26 +0100

    Create ASH_now.sql

---
+++ performances/ash/ASH_now.sql
@@ -0,0 +1,20 @@
+select TO_CHAR(sysdate,'YYYY-MM-DD HH24:MI:SS') current_time from dual;
+
+COL username FORMAT A20
+COL wait_class FORMAT A20
+COL program FORMAT A45
+
+-- high level snapper
+@@snapper ash 5 1 all@*
+
+-- high level wait class and event
+@@ashtop wait_class,event session_type='FOREGROUND' sysdate-5/24/60 sysdate
+
+-- high level per node, wait class and event
+@@ashtop inst_id,wait_class,event,username,sql_id session_type='FOREGROUND' sysdate-5/24/60 sysdate
+
+-- session and sqls
+@@ashtop inst_id,session_id,username,program,sql_id,sql_plan_hash_value,plsql_entry_object_id,blocking_session,event session_type='FOREGROUND' sysdate-5/24/60 sysdate
+
+-- to get what part of the execution plan the SQL is spending most of its time 
+@@ashtop inst_id,session_id,username,program,sql_id,sql_plan_hash_value,plsql_entry_object_id,sql_plan_operation,sql_plan_options,sql_plan_line_id session_type='FOREGROUND' sysdate-5/24/60 sysdate
