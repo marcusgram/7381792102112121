7de9cc3b587c2226bb190d8fc15e842b84f2b093
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-24 10:26:46 +0100

    Update SESSION_Kill.sql

--- administration/sessions/SESSION_Kill.sql
+++ administration/sessions/SESSION_Kill.sql
@@ -198,6 +198,43 @@ kill -9 SPID
 
 
 
+
+---------------------------------------
+-- Generate kill session for a username
+---------------------------------------
+SELECT 'alter system kill session '''||s.sid||','||s.serial#||''';'
+FROM v$session s
+WHERE s.username = '&user' and s.terminal is NULL
+/
+
+
+----------------------------------
+-- Generate kill session for a job
+----------------------------------
+select spid, 'alter system kill session '''||sid||','||serial#||''';' as killscript, calisankod from
+( SELECT /*+ RULE */
+         p.spid
+        ,r.sid
+        ,v.serial#
+        ,r.job
+        ,Substr(j.schema_user,1,12) as USERNAME
+        ,r.this_date
+        ,r.this_sec
+        ,j.failures
+        ,j.broken
+        ,Substr(j.interval,1,40) as periyod
+        ,Substr(v.program,1,30) as program
+        ,Substr(j.what,1,200) as calisankod
+  FROM dba_jobs_running r, dba_jobs j, v$session v, v$process p
+  WHERE r.job = j.job
+    AND r.sid = v.sid
+    AND v.paddr = p.addr(+) )
+where upper(calisankod) like upper('%&kod%')
+/
+
+
+
+
 -----------------------------------------------	  
 -- BLOC PL/SQL  --
 -- Supprimer en auto toutes les sessions 
