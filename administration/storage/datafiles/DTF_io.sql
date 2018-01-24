b309b75e6daaedec33d565fda36620d44ed98764
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-24 10:13:53 +0100

    Update DTF_io.sql

--- administration/storage/datafiles/DTF_io.sql
+++ administration/storage/datafiles/DTF_io.sql
@@ -13,6 +13,29 @@ WHERE df.file# = fs.file#
 ORDER by c desc
 /
 
+
+
+
+SELECT Substr(df.name,2,8) as filesystem,
+       Sum(fs.phyrds) as reads,
+       Sum(fs.phywrts) as writes,
+       Sum(fs.phyrds+fs.phywrts) as readwrites
+FROM v$datafile df, v$filestat fs
+WHERE df.file# = fs.file#
+GROUP BY Substr(df.name,2,8)
+UNION ALL
+SELECT Substr(df.name,2,8) as filesystem,
+       Sum(fs.phyrds) as reads,
+       Sum(fs.phywrts) as writes,
+       Sum(fs.phyrds+fs.phywrts) as readwrites
+FROM v$tempfile df, v$filestat fs
+WHERE df.file# = fs.file#
+GROUP BY Substr(df.name,2,8)
+ORDER BY 4 desc
+/
+
+
+
 ---------------------------
 -- Get io for a filename --
 ---------------------------
