c672c8c54a7f5b484de0150d3fe5d38509233b76
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-24 12:50:32 +0100

    Update SESSION_Locks.sql

--- administration/sessions/SESSION_Locks.sql
+++ administration/sessions/SESSION_Locks.sql
@@ -23,3 +23,21 @@ ORDER BY Substr(owner,1,10) ASC,
          Substr(object_name,1,20) ASC,
          NVL( lockwait, 'ACTIVE') DESC
 /
+
+
+
+
+column lock_type format a12
+column mode_held format a10
+column mode_requested format a10
+column blocking_others format a20
+column username format a10
+SELECT	session_id
+,	lock_type
+,	mode_held
+,	mode_requested
+,	blocking_others
+,	lock_id1
+FROM	dba_lock l
+WHERE 	lock_type NOT IN ('Media Recovery', 'Redo Thread')
+/
