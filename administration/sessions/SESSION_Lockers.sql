e1466da503f9e22f833c0f49330478e4cebc3c17
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-24 10:35:45 +0100

    Update SESSION_Lockers.sql

--- administration/sessions/SESSION_Lockers.sql
+++ administration/sessions/SESSION_Lockers.sql
@@ -67,6 +67,22 @@ where a.sid in ( select session_id from v$locked_object )
   and a.lockwait != 'ACTIVE'
 /
 
+
+-----------------------------------------
+-- Generate kill according to locked object
+-----------------------------------------
+select 'alter system kill session '''||sid||','||serial#||''';' 
+from v$session
+where sid in ( SELECT s.sid from v$process p, v$session s, dba_ddl_locks d
+               WHERE s.paddr = p.addr(+)
+                 AND s.username is not null
+                 AND s.sid = d.session_id
+                 AND d.name = Upper('&objname')
+                 AND upper(s.osuser) not like '%AOZSOYLU%'
+                 AND Substr(upper(s.terminal),1,8) != 'ATILLAOZ' )
+/
+
+
 ---------------------------------------------------------------
 -- To get limited information on blocking sessions you can use:
 ---------------------------------------------------------------
