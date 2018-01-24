ba76b5033422720e1d68c6237d7bada01a48fa7e
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-24 10:54:58 +0100

    Update SESSION_Lockers.sql

--- administration/sessions/SESSION_Lockers.sql
+++ administration/sessions/SESSION_Lockers.sql
@@ -44,7 +44,26 @@ FROM v$lock l1,
 WHERE l1.block = 1
 AND l2.request > 0
 AND l1.id1     = l2.id1
-AND l1.id2     = l2.id2;
+AND l1.id2     = l2.id2
+/
+
+
+
+SELECT
+  p.spid unix_pid,
+  s.sid,
+  s.serial#,
+  s.status,
+  Substr(s.username,1,10) "Username",
+  s.terminal,
+  s.osuser,
+  s.last_call_et "Idle seconds",
+  s.program "Program"
+FROM v$process p, v$session s
+WHERE s.paddr   = p.addr(+)
+  and s.sid in ( select sid from v$lock where block=1 union all select sid from v$lock where request > 0 )
+ORDER BY s.username, s.terminal
+/
 
 
 
