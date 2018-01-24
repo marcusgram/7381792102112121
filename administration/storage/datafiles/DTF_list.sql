1e0e88034bc8f1e0eaa65ba31601099c07b6a6ce
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-24 09:48:52 +0100

    Update DTF_list.sql

--- administration/storage/datafiles/DTF_list.sql
+++ administration/storage/datafiles/DTF_list.sql
@@ -1,5 +1,5 @@
 
-
+rem ------------------------------------------------
 select bytes "MB", status, file#, block_size, name, creation_time from
 ( select '1' as sira, status, file#, trunc(bytes/(1024*1024)) as bytes, block_size, name, creation_time
     from v$datafile
@@ -11,3 +11,43 @@ select bytes "MB", status, file#, block_size, name, creation_time from
     from v$tempfile )
 order by sira, name, file#
 /
+rem ------------------------------------------------
+
+rem ------------------------------------------------
+select tablespace_name,
+       (bytes/1048576) as filesize,
+       (maxbytes/1048576) as maxsize,
+       DECODE(autoextensible,'YES','+',' ') as auto,
+       file_name
+from dba_data_files
+order by tablespace_name, file_name
+/
+rem -------------------------------------------------
+
+rem -------------------------------------------------
+select tablespace_name,
+       (bytes/1048576) as filesize,
+       (maxbytes/1048576) as maxsize,
+       DECODE(autoextensible,'YES','+',' ') as auto,
+       file_name
+from dba_data_files
+where Upper(file_name) like Upper('%&filename%')
+order by tablespace_name, file_name
+/
+rem ------------------------------------------------
+
+rem -------------------------------------------------
+select tablespace_name,
+       (bytes/1048576) as filesize,
+       DECODE(autoextensible,'YES','+',' ') as auto,
+       file_name
+from dba_data_files
+where tablespace_name like Upper('&tablespacename'||'%')
+order by tablespace_name, file_name
+/
+rem -------------------------------------------------
+
+rem -------------------------------------------------
+
+
+
