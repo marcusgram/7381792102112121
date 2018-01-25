5d863404cca3b6a4f9c3c8b2617cd0e87e879162
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-25 13:57:40 +0100

    Update ASH_TOP_SQL_Ressources.sql

--- performances/ash/ASH_TOP_SQL_Ressources.sql
+++ performances/ash/ASH_TOP_SQL_Ressources.sql
@@ -5,6 +5,10 @@
  sum(decode(ash.session_state,'WAITING', decode(en.wait_class, 'User I/O',1,0),0))    "IO" ,      
  sum(decode(ash.session_state,'ON CPU',1,1)) "TOTAL" 
  from v$active_session_history ash,        
- v$event_name en where SQL_ID is not NULL   
- and en.event#=ash.event# group by sql_id 
- order by sum(decode(session_state,'ON CPU',1,1))   desc 
+ v$event_name en 
+ where SQL_ID is not NULL   
+ and en.event#=ash.event# 
+ group by sql_id 
+ order by sum(decode(session_state,'ON CPU',1,1)) desc 
+/
+
