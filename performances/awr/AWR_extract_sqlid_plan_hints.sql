487a37958b0275902e173a05fbe33c8c42fd9c09
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-25 09:28:01 +0100

    Create AWR_extract_sqlid_plan_hints.sql

---
+++ performances/awr/AWR_extract_sqlid_plan_hints.sql
@@ -0,0 +1,19 @@
+
+
+-- Extracts plan hints from AWR
+
+select
+extractvalue(value(d), '/hint') as outline_hints
+from
+xmltable('/*/outline_data/hint'
+passing (
+select
+xmltype(other_xml) as xmlval
+from
+dba_hist_sql_plan
+where
+sql_id = '&sql_id'
+and plan_hash_value = nvl('&plan_hash_value',plan_hash_value)
+and other_xml is not null
+)
+) d;
