1b9e2f1f2c6662b13573d10a939e2eccb06988b6
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-01-31 12:40:31 +0100

    Update and rename SQLID_Get_bind_value.sql to SQLID_Get_bind_variable_value.sql

--- performances/sql/SQLID_Get_bind_variable_value.sql
+++ performances/sql/SQLID_Get_bind_variable_value.sql
@@ -8,3 +8,14 @@
 select * from gV$SQL_BIND_CAPTURE where sql_id='&1';
 
 select BINDS_XML from gv$sql_monitor where sql_id='&1';
+
+
+set linesize 512
+set pages 50000
+col LAST_CAPTURED format a30
+col VALUE_STRING  format a50
+
+select snap_id,to_char(LAST_CAPTURED,'YYYY-MM-DD HH24:MI:SS') LAST_CAPTURED,WAS_CAPTURED,NAME,VALUE_STRING 
+from DBA_HIST_SQLBIND
+where sql_id='&sql_id'
+order by LAST_CAPTURED,NAME;
