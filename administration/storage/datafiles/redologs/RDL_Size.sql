673959cbb654b2cf31442ee398dd634ded400247
marcusgram <35613686+marcusgram@users.noreply.github.com>
2018-02-15 12:39:18 +0100

    Update RDL_Size.sql

--- administration/storage/datafiles/redologs/RDL_Size.sql
+++ administration/storage/datafiles/redologs/RDL_Size.sql
@@ -8,6 +8,19 @@ AND       FIRST_TIME > SYSDATE - interval '&days' day
 GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY')
 ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'));
 
+/*
+Date                                                 Total Redo (GB)
+---------------------------- ---------------------------------------
+10-FÉVR.-2018                                                6.8E-01
+11-FÉVR.-2018                                                  1,125
+12-FÉVR.-2018                                                5.9E+00
+13-FÉVR.-2018                                                1.1E+00
+14-FÉVR.-2018                                                1.1E+00
+15-FÉVR.-2018                                                3.8E-01
+*/
+
+
+
 -- Query to obtain redo generation per hour.
 SET PAGESIZE 1000 LINESIZE 155
 BREAK ON DAY SKIP 1
@@ -18,6 +31,18 @@ WHERE     DEST_ID = 1
 AND       FIRST_TIME > SYSDATE - interval '&hours' hour
 GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'), TO_CHAR(FIRST_TIME, 'HH24')
 ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY')), TO_CHAR(FIRST_TIME, 'HH24');
+
+/*
+Day                          Ho                         Total Redo (GB)
+---------------------------- -- ---------------------------------------
+15-FÉVR.-2018                07                                 1.4E-02
+15-FÉVR.-2018                08                                 2.5E-02
+15-FÉVR.-2018                09                                 2.7E-02
+15-FÉVR.-2018                10                                 4.4E-02
+15-FÉVR.-2018                11                                 3.0E-02
+*/
+
+
 
 -- Query to obtain redo generation per minute.
 SET PAGESIZE 1000 LINESIZE 155
